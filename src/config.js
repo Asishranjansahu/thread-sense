f287a6cbe15ec303648c6a8909df8cf47e7a6bcd
Asish Ranjan Sahu <Asishranjansahu@users.noreply.github.com>
2026-02-06 03:01:10 +0530

    Config: Switch to Production API URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-export const API = "http://localhost:5050";
+export const API = import.meta.env.PROD
+    ? "https://thread-sense.onrender.com"
+    : "http://localhost:5050";
 export const GOOGLE_CLIENT_ID = "1069871585161-2msu0pn40pkimlk1ongbasesjfbl2qf2.apps.googleusercontent.com";
